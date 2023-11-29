47eccc45252e974b695c615cc0c0b164e40c9c7c
Manikgog <m4xin4t4@gmail.com>
2023-11-29 12:41:22 +0500

    Initial commit

---
+++ src/test/java/ru/examination/examination_application/ExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.examination.examination_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
